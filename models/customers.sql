aa6999957583fe98f652319fd0d28501c41fcb30
Zachary <zachary.friedman@immuta.com>
2021-04-16 15:04:06 -0400

    Add ephemeral model

--- models/customers.sql
+++ models/customers.sql
@@ -18,15 +18,7 @@ payments as (
 
 customer_orders as (
 
-        select
-        customer_id,
-
-        min(order_date) as first_order,
-        max(order_date) as most_recent_order,
-        count(order_id) as number_of_orders
-    from orders
-
-    group by 1
+    select * from {{ ref('stg_customer_orders') }}
 
 ),
 
---
+++ models/staging/stg_customer_orders.sql
@@ -0,0 +1,23 @@
+{{ config(materialized='ephemeral') }}
+
+with orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+customer_orders as (
+
+    select
+        customer_id,
+
+        min(order_date) as first_order,
+        max(order_date) as most_recent_order,
+        count(order_id) as number_of_orders
+    from orders
+
+    group by 1
+
+)
+
+select * from customer_orders
