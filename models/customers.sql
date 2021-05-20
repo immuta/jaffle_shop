4ce5c1b300aad5ab8085eca3451c3f84f12d0aa8
Zachary <zachary.friedman@immuta.com>
2021-05-20 17:19:21 -0400

    Add first_name and last_name to customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -40,6 +40,8 @@ final as (
 
     select
         customers.customer_id,
+        customers.first_name,
+        customers.last_name,
         customer_orders.first_order,
         customer_orders.most_recent_order,
         customer_orders.number_of_orders,
